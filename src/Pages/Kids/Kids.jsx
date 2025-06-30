b665ad8fbfd1bb8f037e53b04417bc21e0885088
Mohd-Shanawaz <mshanawaz914@gmail.com>
2025-06-30 16:01:14 +0530

    Updated images

--- src/Pages/Kids/Kids.jsx
+++ src/Pages/Kids/Kids.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Kids.css"; // Import the CSS file
 
 const kidsProducts = [
-  { id: 1, name: "Cartoon T-Shirt", price: "$19", image: "https://via.placeholder.com/200x250?text=T-Shirt" },
-  { id: 2, name: "Toy Car", price: "$29", image: "https://via.placeholder.com/200x250?text=Toy+Car" },
-  { id: 3, name: "Kids Sneakers", price: "$39", image: "https://via.placeholder.com/200x250?text=Sneakers" },
-  { id: 4, name: "Backpack", price: "$25", image: "https://via.placeholder.com/200x250?text=Backpack" },
+  { id: 1, name: "Cartoon T-Shirt", price: "$19", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/kids-t-shirt/q/4/m/5-6-years-doraemon-group-maadhav-fashions-original-imahbdun4gjf42x2.jpeg?q=70" },
+  { id: 2, name: "Toy Car", price: "$29", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/remote-control-toy/6/8/f/remote-controlled-ferrari-with-opening-doors-3-webby-original-imahfet874duz39m.jpeg?q=70" },
+  { id: 3, name: "Kids Sneakers", price: "$39", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/kids-shoe/1/o/l/13c-wave001-hooh-now-comfort-in-fashion-original-imahd6bybuv3fqjn.jpeg?q=70" },
+  { id: 4, name: "Backpack", price: "$25", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/backpack/2/n/i/kids-stylish-junior-school-bag-backpacks-cartoon-boy-girl-baby-3-original-imagj7efhm7czz2k.jpeg?q=70" },
 ];
 
 const Kids = () => {
--- src/Pages/Mens/Mens.jsx
+++ src/Pages/Mens/Mens.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Mens.css"
 
 const products = [
-  { id: 1, name: "Casual Shirt", price: "$49", image: "https://via.placeholder.com/200x250?text=Shirt" },
-  { id: 2, name: "Denim Jeans", price: "$79", image: "https://via.placeholder.com/200x250?text=Jeans" },
-  { id: 3, name: "Leather Jacket", price: "$199", image: "https://via.placeholder.com/200x250?text=Jacket" },
-  { id: 4, name: "Running Shoes", price: "$129", image: "https://via.placeholder.com/200x250?text=Shoes" },
+  { id: 1, name: "Casual Shirt", price: "$49", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shirt/i/o/8/m-nt-121-ziczac-urban-buccachi-original-imah9h6msefszqgc.jpeg?q=70" },
+  { id: 2, name: "Denim Jeans", price: "$79", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/jean/o/y/i/38-avdisheavywhsk-urbano-fashion-original-imah3gn9amsfgqb2.jpeg?q=70" },
+  { id: 3, name: "Leather Jacket", price: "$199", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/jacket/p/p/l/m-1-no-leather-zyrona-original-imah8wg6bgggkghu.jpeg?q=70" },
+  { id: 4, name: "Running Shoes", price: "$129", image: "https://rukminim2.flixcart.com/image/612/612/xif0q/shoe/q/e/s/12-22g-1275-campus-blk-silver-original-imahbbhwgz9j3fr5.jpeg?q=70" },
 ];
 
 const Mens = () => {
